9e230fc9d2cec18318b2a6dcd5d19480e11ded7f
Egor <egor.rumyancev.9494@mail.ru>
2021-04-14 16:13:08 +0500

    Finish

---
+++ Task2/Task2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e, x;
+		cout << "Write numbers with a space for example (((a + b) * c) - d) / e" << endl;
+		cin >> a >> b >> c >> d >> e;
+	
+		_asm
+		{
+			mov eax, a;  // a => eax служит нам "хранилищем", куда мы можем записывать всякие данные! 
+			add eax, b   // (a + b)
+			mul c;	     // ((a + b) * c)
+			sub eax, d;  // (((a + b) * c) - d)
+			div e;		 // (((a + b) * c) - d) / e
+			mov x, eax;  // x = (((a + b) * c) - d) / e
+		}
+		cout << "Get result: " << x << endl;
+}
